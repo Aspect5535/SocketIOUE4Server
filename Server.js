46066b7fbff9cbda3044cb12effae409e679e38b
Aspect5535 <150635438+Aspect5535@users.noreply.github.com>
2026-07-24 21:24:33 -0400

    yes

--- Server.js
+++ Server.js
@@ -432,6 +432,6 @@ socket.on("npcMove", (data) => {
 // Start
 // ------------------------------------------------------------
 const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, "0.0.0.0", () => {
   console.log(`Socket.IO server running on port ${PORT}`);
 });
